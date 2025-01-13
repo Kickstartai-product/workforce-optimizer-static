5e0271d48eb097ad705f194658a4d033ac884427
ICascha <caschavanwanrooij@gmail.com>
2025-01-13 15:19:37 +0100

    add gh actions file

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/workforce-optimizer-static/' : '/',
   plugins: [react(),     
   compression({
     algorithm: 'gzip',  // or 'brotliCompress', 'deflate', etc.
