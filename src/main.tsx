345572bbba541e1cf127c21dab131d6b7cc4af7b
ICascha <caschavanwanrooij@gmail.com>
2025-01-25 13:52:33 +0100

    use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
-   <BrowserRouter>
+   <HashRouter>
      <Routes>
        <Route path="/" element={<App />} />
        <Route path="/arbeidsmarkt-transitiemodel" element={<App />} />
      </Routes>
-   </BrowserRouter>
+   </HashRouter>
  </StrictMode>,
 )
